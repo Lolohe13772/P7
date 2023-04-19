06bc2b7cf6507b426552833aef3d8e62c9596721
Laurent HENON <laurenthenon@MacBook-Air-de-Laurent.local>
2023-04-19 11:57:58 +0200

    Sauvegarde de plein de choses

--- back/api.py
+++ back/api.py
@@ -1,14 +1,29 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+import pandas as pd
+from numpy import array
+import pickle
+NAME_COLS_TRAIN =['BURO_DAYS_CREDIT_MEAN', 'DAYS_BIRTH', 'PREV_NAME_CONTRACT_STATUS_Refused_MEAN', 'BURO_DAYS_CREDIT_MIN', 'CLOSED_DAYS_CREDIT_MIN', 'CLOSED_DAYS_CREDIT_MEAN', 'BURO_DAYS_CREDIT_UPDATE_MEAN', 'CC_CNT_DRAWINGS_ATM_CURRENT_MEAN', 'REGION_RATING_CLIENT_W_CITY']
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Charger le modèle entraîné
+with open('../Data/model.pickle', 'rb') as f:
+    model = pickle.load(f)
 
-@app.route("/getclient")
-def getclient():
-    return [1,2,3,4,8]
+# Charger le dataframe df_scoring.csv
+df = pd.read_csv('../Data/df_scoring.csv')
+df = df.set_index('SK_ID_CURR')
 
-app.run()
 
+# Définition de la route pour prédire la solvabilité d'un client
+@app.route('/predict_solvabilite/<int:id_client>')
+def predict_solvabilite(id_client):
+    print(id_client)
+    info_client=df.loc[id_client]
+    useful_data=array(info_client[NAME_COLS_TRAIN])
+    useful_data= useful_data.reshape(1, -1)
+    target=model.predict_proba(useful_data)[0][0]
+    return str(target)
+
+# Lancement de l'application Flask
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8050)
--- front/front.py
+++ front/front.py
@@ -1,1 +1,47 @@
-print("Hello World")
+import dash
+from dash import dcc
+from dash import html, Input, Output
+import requests
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+# Chargement des identifiants clients depuis le fichier csv
+df = pd.read_csv("df_scoring.csv")
+clients = [{'label': str(client), 'value': client} for client in df["SK_ID_CURR"]]
+
+app.layout = html.Div([
+    html.H1("Prêt à dépenser - Etude de solvabilité"),
+    html.Div([
+        html.Div([
+            html.Label('Sélectionnez un client dans le menu déroulant ci-dessous : '),
+            dcc.Dropdown(
+                id='client-dropdown',
+                options=clients,
+                value=clients[0]['value']
+            )
+        ], className='three columns'),
+        html.Div([
+            html.Label('Résultat : '),
+            html.Div(id='solvabilite')
+        ], className='nine columns')
+    ], className='row')
+])
+
+   
+# Callback pour mettre à jour la solvabilité affichée lorsque l'utilisateur sélectionne un client dans le menu déroulant
+@app.callback(
+    Output('solvabilite', 'children'),
+    Input('client-dropdown', 'value'))
+def get_solvabilite(id_client):
+    url = f"http://127.0.0.1:8050/predict_solvabilite/{id_client}"
+    response = requests.get(url)
+    print (response)
+    solvabilite = float(response.text)
+    if solvabilite >= 0.5:
+        return f"Le client est solvable: un prêt peut lui être accordé(solvabilité:{solvabilite})"
+    else:
+        return f"Le client n'est pas solvable: il est impossible de lui octroyer un emprunt, risque de perte en capital(solvabilité:{solvabilite})"
+    
+if __name__ == '__main__':
+    app.run_server(port=8080)
