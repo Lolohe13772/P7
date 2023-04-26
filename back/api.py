c5f60cad425f14b9e8848c83cdb06ec9c179d737
Lolohe13772 <lolohe13772@gmail.com>
2023-04-26 09:19:58 +0200

    J'ai modifié l'API

--- back/api.py
+++ back/api.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 import pandas as pd
 from numpy import array
 import pickle
+import io
+from flask import send_file
 NAME_COLS_TRAIN =['BURO_DAYS_CREDIT_MEAN', 'DAYS_BIRTH', 'PREV_NAME_CONTRACT_STATUS_Refused_MEAN', 'BURO_DAYS_CREDIT_MIN', 'CLOSED_DAYS_CREDIT_MIN', 'CLOSED_DAYS_CREDIT_MEAN', 'BURO_DAYS_CREDIT_UPDATE_MEAN', 'CC_CNT_DRAWINGS_ATM_CURRENT_MEAN', 'REGION_RATING_CLIENT_W_CITY']
 app = Flask(__name__)
 
@@ -13,7 +15,6 @@ with open('../Data/model.pickle', 'rb') as f:
 df = pd.read_csv('../Data/df_scoring.csv')
 df = df.set_index('SK_ID_CURR')
 
-
 # Définition de la route pour prédire la solvabilité d'un client
 @app.route('/predict_solvabilite/<int:id_client>')
 def predict_solvabilite(id_client):
@@ -23,6 +24,43 @@ def predict_solvabilite(id_client):
     useful_data= useful_data.reshape(1, -1)
     target=model.predict_proba(useful_data)[0][0]
     return str(target)
+
+# Définition de la route pour récupérer les données d'âge du client
+@app.route('/age_client/<int:id_client>')
+def age_client(id_client):
+    info_client = df.loc[id_client]
+    age = info_client['DAYS_BIRTH'] / -365 # Convertir les jours en années
+    return jsonify({'age': age})
+
+# Définition de la route pour récupérer le statut salarié du client
+@app.route('/statut_salarie/<int:id_client>')
+def statut_salarie(id_client):
+    info_client = df.loc[id_client]
+    statut = "salarié" if info_client['NAME_INCOME_TYPE_Working'] == 1 else "non salarié"
+    return jsonify({'statut': statut})
+
+# Définition de la route pour afficher les données d'un client
+@app.route('/client/<int:id_client>')
+def afficher_client(id_client):
+    info_client = df.loc[id_client] # Récupérer les informations sur le client
+    age = info_client['DAYS_BIRTH'] / -365 # Calculer l'âge du client
+    statut_salarie = "salarié" if info_client['NAME_INCOME_TYPE_Working'] == 1 else "non salarié" # Déterminer le statut salarié du client
+
+    # Créer un dictionnaire contenant les données du client à afficher
+    data = {
+        'age': age,
+        'statut_salarie': statut_salarie,
+        'BURO_DAYS_CREDIT_MEAN': info_client['BURO_DAYS_CREDIT_MEAN'],
+        'PREV_NAME_CONTRACT_STATUS_Refused_MEAN': info_client['PREV_NAME_CONTRACT_STATUS_Refused_MEAN'],
+        'BURO_DAYS_CREDIT_MIN': info_client['BURO_DAYS_CREDIT_MIN'],
+        'CLOSED_DAYS_CREDIT_MIN': info_client['CLOSED_DAYS_CREDIT_MIN'],
+        'CLOSED_DAYS_CREDIT_MEAN': info_client['CLOSED_DAYS_CREDIT_MEAN'],
+        'BURO_DAYS_CREDIT_UPDATE_MEAN': info_client['BURO_DAYS_CREDIT_UPDATE_MEAN'],
+        'CC_CNT_DRAWINGS_ATM_CURRENT_MEAN': info_client['CC_CNT_DRAWINGS_ATM_CURRENT_MEAN'],
+        'REGION_RATING_CLIENT_W_CITY': info_client['REGION_RATING_CLIENT_W_CITY']
+    }
+
+    return jsonify(data) # Renvoyer les données au format JSON
 
 # Lancement de l'application Flask
 if __name__ == '__main__':
--- front/front.py
+++ front/front.py
@@ -1,6 +1,5 @@
 import dash
-from dash import dcc
-from dash import html, Input, Output
+from dash import dcc, html, Input, Output
 import requests
 import pandas as pd
 
@@ -11,10 +10,10 @@ df = pd.read_csv("df_scoring.csv")
 clients = [{'label': str(client), 'value': client} for client in df["SK_ID_CURR"]]
 
 app.layout = html.Div([
-    html.H1("Prêt à dépenser - Etude de solvabilité"),
+    html.H1("Prêt à dépenser - Etude de solvabilité", style={'text-align': 'center'}),
     html.Div([
         html.Div([
-            html.Label('Sélectionnez un client dans le menu déroulant ci-dessous : '),
+            html.Label('Sélectionnez un identifiant client dans le menu déroulant ci-dessous : '),
             dcc.Dropdown(
                 id='client-dropdown',
                 options=clients,
@@ -25,10 +24,12 @@ app.layout = html.Div([
             html.Label('Résultat : '),
             html.Div(id='solvabilite')
         ], className='nine columns')
-    ], className='row')
+    ], className='row'),
+    html.Div([
+        dcc.Graph(id='age-graph')
+    ])
 ])
 
-   
 # Callback pour mettre à jour la solvabilité affichée lorsque l'utilisateur sélectionne un client dans le menu déroulant
 @app.callback(
     Output('solvabilite', 'children'),
@@ -39,9 +40,48 @@ def get_solvabilite(id_client):
     print (response)
     solvabilite = float(response.text)
     if solvabilite >= 0.5:
-        return f"Le client est solvable: un prêt peut lui être accordé(solvabilité:{solvabilite})"
+        return f"Le client est solvable: un prêt peut lui être accordé (solvabilité:{solvabilite})"
     else:
-        return f"Le client n'est pas solvable: il est impossible de lui octroyer un emprunt, risque de perte en capital(solvabilité:{solvabilite})"
+        return f"Le client n'est pas solvable: il est impossible de lui octroyer un emprunt, risque de perte en capital (solvabilité:{solvabilite})"
     
+@app.callback(
+    Output('age-graph', 'figure'),
+    Input('client-dropdown', 'value'))
+def update_age_graph(id_client):
+    # Récupérer l'âge du client depuis le dataframe
+    age = df[df["SK_ID_CURR"] == id_client]["DAYS_BIRTH"].values[0] / -365.25
+    
+    # Déterminer la colonne de l'âge du client sélectionné
+    age_col = int(age // 10)
+    
+    # Créer les colonnes pour le graphique
+    x = [f"{i * 10}-{i * 10 + 9}" for i in range(0, 10)]
+    y = [len(df[(df["DAYS_BIRTH"] / -365.25 >= i * 10) & (df["DAYS_BIRTH"] / -365.25 < i * 10 + 10)]) for i in range(0, 10)]
+    
+    # Modifier la couleur de la colonne de l'âge du client sélectionné
+    y[age_col] = {'y': y[age_col], 'fillcolor': '#FFA07A'}
+    
+    # Créer le graphique
+    fig = {
+        'data': [{
+            'x': x,
+            'y': y,
+            'type': 'bar',
+            'marker': {'color': '#1E90FF'}
+        }],
+        'layout': {
+            'title': 'Répartition des âges des clients',
+            'annotations': [
+                {'x': age_col, 'y': y[age_col]['y'], 'text': f"Âge du client identifié: {int(age)} ans", 
+                 'font': {'color': '#FFA07A'}, 'showarrow': False, 'yanchor': 'bottom'}
+            ],
+            'xaxis': {'title': 'Tranches d\'âge (années)'},
+            'yaxis': {'title': 'Nombre de clients'},
+            'barmode': 'stack'
+        }
+    }
+    
+    return fig
+
 if __name__ == '__main__':
     app.run_server(port=8080)
